b46ea2b008c5b0ccdd684c6535d742c5d632aa76
Syazwan <syazwan.mdsani@24mas.com>
2011-07-22 10:53:23 +0800

    Add Greek translation by panos78

---
+++ lang/el.php
@@ -0,0 +1,13 @@
+<?php
+$translations = array (
+    'AdminOverview_PluginDescription'   => 'Επισκόπηση ιστοσελίδων και χρηστών',
+    'AdminOverview_MenuOverview'        => 'Επισκόπηση',
+    'AdminOverview_UserAccessDescription' => 'Έχετε %d χρήστες και %d ιστσσελίδες.',
+    'AdminOverview_User'                => 'Χρήστης',
+    'AdminOverview_ViewCount'           => 'Προβληθείσες ιστοσελίδες',
+    'AdminOverview_AdminCount'          => 'Διαχειριζόμενες ιστοσελίδες',
+    'AdminOverview_AccessCount'         => 'Συνολική πρόσβαση',
+    'AdminOverview_UserSitesDescription' => 'Πληροφορίες χρήστη',
+    'AdminOverview_Site'                => 'Ονομασία Ιστοσελίδας',
+    'AdminOverview_Access'              => 'Επίπεδο πρόσβασης',
+);
